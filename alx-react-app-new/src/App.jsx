c0589fd4477e90df1eb48c191dff475ba345c440
bezaMesfin16 <Mesfinbeza16@gmail.com>
2026-01-18 21:25:22 +0300

    counter created

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -3,6 +3,7 @@ import UserProfile from './components/UserProfile';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
+import counter from './components/Counter';
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
@@ -20,6 +21,7 @@ function App() {
                 bio="Loves hiking and photography" 
             />
             {/* You can add more UserProfile components if needed */}
+            <Counter />
             <Footer />
         </div>
     );
